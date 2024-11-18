a39ace21e37f2ec84e284869e3e333510e4689c5
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-18 20:01:15 +0100

    logic added and tests passed

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -4,7 +4,19 @@ package org.factoriaf5.rna_transcription;
 public class RnaTranscription {
     
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
+        String rnaStrand = "";
+        if(dnaStrand.matches("(C|G|T|A)+")){
+            for (char c:dnaStrand.toCharArray()){
+                rnaStrand+=c=='G'?
+                'C':
+                c=='C'?
+                'G':
+                c=='T'?
+                'A':
+                'U';
+            }
+        }
+        return rnaStrand;
     }
 
 }
--- src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
+++ src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
@@ -22,31 +22,26 @@ public class RnaTranscriptionTest {
         assertThat(rnaTranscription.transcribe("").isEmpty(), is(true));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfCytosineIsGuanine() {
         assertThat(rnaTranscription.transcribe("C"), is(equalTo("G")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
         assertThat(rnaTranscription.transcribe("G"), is(equalTo("C")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
         assertThat(rnaTranscription.transcribe("T"), is(equalTo("A")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
         assertThat(rnaTranscription.transcribe("A"), is(equalTo("U")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscription() {
         assertThat(rnaTranscription.transcribe("ACGTGGTCTTAA"), is(equalTo("UGCACCAGAAUU")));
